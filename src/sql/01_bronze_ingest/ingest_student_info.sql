e27ab32c8fe7df803f8115e70a16df5f34fcb6da
alleanna.villan@ftwfoundation.org <alleanna.villan@ftwfoundation.org>
2026-09-06 13:56:27 +0000

    ingestions

--- src/sql/01_bronze_ingest/ingest_student_info.sql
+++ src/sql/01_bronze_ingest/ingest_student_info.sql
@@ -1,1 +1,8 @@
+CREATE TABLE IF NOT EXISTS oulad.oulad_bronze.student_info_bronze
+USING DELTA
+AS
+SELECT *,
+  current_timestamp() AS ingestion_timestamp,
+  current_date() AS ingestion_date
+FROM read_files('/Volumes/oulad/default/ftw-b12-de-r2/shared/week07/studentInfo.csv');
 
